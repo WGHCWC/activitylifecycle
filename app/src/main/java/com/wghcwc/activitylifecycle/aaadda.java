8c6f6402efd76bfc1b5daa9ee0dbd25dc45df89b
WEICHEN <wghcghost@hotmail.com>
2020-02-06 20:15:10 +0800

    ktx

---
+++ app/src/main/java/com/wghcwc/activitylifecycle/aaadda.java
@@ -0,0 +1,16 @@
+package com.wghcwc.activitylifecycle;
+
+import com.wghcwc.activitylifecycle2.ActivityLifecycle;
+
+/**
+ * @author wghcwc
+ * @date 20-2-6
+ */
+public class aaadda {
+
+
+    public void aa(){
+        ActivityLifecycle.init(ActivityLifecycle.getApp());
+    }
+
+}
